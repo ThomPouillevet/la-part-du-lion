f1f9e33f406e882eb9c2653fc38f2d2e8b3b07bd
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2020-04-16 21:11:09 +0200

    ✨ Adds 404, starters, dishes and desserts pages.

---
+++ src/pages/desserts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const desserts = () => {
+  return (
+    <div>
+      <h1>Desserts page</h1>
+    </div>
+  )
+}
+
+export default desserts
---
+++ src/pages/starters.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const starters = () => {
+  return (
+    <div>
+      <h1>Starters page</h1>
+    </div>
+  )
+}
+
+export default starters
