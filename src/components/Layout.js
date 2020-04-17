dda17d0c224b23c5258b7678187198cb0020bd29
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2020-04-17 21:50:42 +0200

    💄 Adds SASS plugin + layout.scss

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "../style/layout.scss"
 
 const Layout = ({ children }) => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default () => <Layout>Hello world!</Layout>
+export default () => (
+  <Layout>
+  <p>Hello world</p>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">Hello</div>
+        <div className="col-4">Hello</div>
+        <div className="col-4">Hello</div>
+      </div>
+    </div>
+  </Layout>
+)
