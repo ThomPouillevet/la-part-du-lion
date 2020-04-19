d8d163af1bd77318a93c9681cf683bf22d18e091
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2020-04-19 11:23:09 +0200

    ➕ Configures gatsby-source-filesystem plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`],
+  siteMetadata: {
+    title: `La Part du Lion`,
+    siteUrl: `https://la-part-du-lion.netlify.app`,
+    description: `Des idées de recettes pendant le confinement`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    }
+  ],
 }
