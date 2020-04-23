f4d8e63809b06d677cbf6619068c1993c2355081
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2020-04-23 22:28:44 +0200

    💄 Improves hero min-height.

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -11,7 +11,7 @@ const StyledHero = ({ img, className, children, home }) => {
 }
 
 export default styled(StyledHero)`
-  min-height: ${props => (props.home ? "calc(100vh - 23rem)" : "50vh")};
+  min-height: ${props => (props.home ? "80vh" : "50vh")};
   background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4));
   background-position: center;
   background-size: cover;
@@ -21,6 +21,6 @@ export default styled(StyledHero)`
   align-items: center;
 
   @media screen and (min-width: 800px) {
-    min-height: ${props => (props.home ? "calc(100vh - 20rem)" : "50vh")};
+    min-height: ${props => (props.home ? "70vh" : "50vh")};
   }
 `
